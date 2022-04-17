051f4cd5b9e842d6079917a6ef2c6342bf12da5a
Aleksey Likhachev <ocmwdt@gmail.com>
2022-04-17 22:03:03 +0400

    add simple property-based tests

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,12 +2,16 @@ package org.example;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public int getClosestEven(int number) {
+        return number % 2 == 0
+                ? number
+                : number - 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Class for property-based testing!");
     }
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,20 +1,46 @@
 package org.example;
 
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+import org.junit.runner.RunWith;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        Assertions.assertTrue( true );
+@RunWith(JUnitQuickcheck.class)
+public class AppTest {
+
+    private final App app = new App();
+
+    @Property
+    public void resultShouldBeEven(int number) {
+
+        var res = app.getClosestEven(number);
+
+        assertEquals(0, res % 2);
+    }
+
+    @Property
+    public void resultShouldBeTheSameForEven(int number) {
+
+        var num = number * 2;
+
+        var res = app.getClosestEven(num);
+
+        assertEquals(res, num);
+    }
+
+    @Property
+    public void resultShouldBeLessForOdd(int number) {
+
+        var num = number * 2 + 1;
+
+        var res = app.getClosestEven(num);
+
+        assertTrue(res < num);
     }
 }
